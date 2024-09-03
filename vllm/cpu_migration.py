c9babcc43a0a871de290e99a46202caa7428425f
jmaksymczuk <jmaksymczuk@habana.ai>
2024-09-03 13:34:31 +0300

    Create dummy habana_frameworks.

--- vllm/cpu_migration.py
+++ vllm/cpu_migration.py
@@ -1,5 +1,20 @@
+import sys
+import types
+
+# Create dummy habana_frameworks
+habana_frameworks = sys.modules['habana_frameworks'] = types.ModuleType('habana_frameworks')
+torch = sys.modules['habana_frameworks.torch'] = types.ModuleType('habana_frameworks.torch')
+core = sys.modules['habana_frameworks.torch.core'] = types.ModuleType('habana_frameworks.torch.core')
+
+habana_frameworks.torch = torch
+torch.core = core
+core.mark_step = lambda: print('calling mark_step')
+
 import habana_frameworks.torch as htorch
 import torch
+
+# torch.hpu = sys.modules['torch.hpu'] = types.ModuleType('torch.hpu')
+# torch.hpu.synchronize = lambda: print('calling synchronize')
 
 class CpuMigration:
     def __init__(self):
