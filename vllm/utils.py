384ec3ea91b41996a08b030ce98f1448a7ddad31
Yanyi Liu <wolfsonliu@163.com>
2024-11-20 12:44:57 +0800

    [Misc] Add __setitem__ for LazyDict (#10469)
    Signed-off-by: Yanyi Liu <wolfsonliu@163.com>

--- vllm/utils.py
+++ vllm/utils.py
@@ -1541,6 +1541,9 @@ class LazyDict(Mapping, Generic[T]):
             self._dict[key] = self._factory[key]()
         return self._dict[key]
 
+    def __setitem__(self, key: str, value: Callable[[], T]):
+        self._factory[key] = value
+
     def __iter__(self):
         return iter(self._factory)
 
