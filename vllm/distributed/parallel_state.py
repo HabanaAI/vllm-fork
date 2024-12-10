10d44162df136816f345bfd76e06c84f023c7b21
jmaksymczuk <jmaksymczuk@habana.ai>
2024-12-10 17:52:22 +0200

    prints for everything

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -524,7 +524,7 @@ class GroupCoordinator:
                                    dtype=torch.long,
                                    device="cpu")
         
-        print(f'SENDING SIZE TENSOR: {size_tensor} from {self.rank} to {self.ranks[dst]}')
+        print(f'SENDING SIZE TENSOR OF SHAPE: {size_tensor.shape} from {self.rank} to {self.ranks[dst]}')
         # Send object size
         htorch.core.mark_step()
         torch.hpu.synchronize()
@@ -533,7 +533,7 @@ class GroupCoordinator:
                                group=self.cpu_group)
         htorch.core.mark_step()
 
-        print(f'SENDING OBJECT: {object_tensor} from {self.rank} to {self.ranks[dst]}')
+        print(f'SENDING OBJECT OF SHAPE: {object_tensor.shape} from {self.rank} to {self.ranks[dst]}')
         # Send object
         htorch.core.mark_step()
         torch.hpu.synchronize()
@@ -562,6 +562,7 @@ class GroupCoordinator:
                                            src=self.ranks[src],
                                            group=self.cpu_group)
         htorch.core.mark_step()
+        print(f'RECV SIZE TENSOR OF SHAPE: {size_tensor.shape} from {self.ranks[src]}')
 
         # Tensor to receive serialized objects into.
         object_tensor = torch.empty(  # type: ignore[call-overload]
@@ -574,6 +575,7 @@ class GroupCoordinator:
                                              src=self.ranks[src],
                                              group=self.cpu_group)
         htorch.core.mark_step()
+        print(f'RECV OBJ TENSOR OF SHAPE: {object_tensor.shape} from {self.ranks[src]}')
 
         assert rank_object == rank_size, (
             "Received object sender rank does not match the size sender rank.")
@@ -723,6 +725,7 @@ class GroupCoordinator:
                 # use group for GPU tensors
                 htorch.core.mark_step()
                 torch.hpu.synchronize()
+                print(f'sending TENSOR WITH SHAPE: {tensor.shape}, TO: {self.ranks[dst]}')
                 torch.distributed.send(tensor,
                                        dst=self.ranks[dst],
                                        group=group)
@@ -788,6 +791,7 @@ class GroupCoordinator:
                                            src=self.ranks[src],
                                            group=group)
                     htorch.core.mark_step()
+                print(f'RECV TENSOR OF SHAPE: {tensor.shape} from {self.ranks[src]}')
                 if use_all_gather:
                     # do the allgather
                     tensor = all_gather_group.all_gather(  # type: ignore
