09e4e603ffb11d3d4bb3486d332ecc037a525e72
Michał Kuligowski <michal.kuligowski@intel.com>
2025-01-30 12:12:54 +0100

    Update weight_utils.py

--- vllm/model_executor/model_loader/weight_utils.py
+++ vllm/model_executor/model_loader/weight_utils.py
@@ -602,13 +602,13 @@ def initialize_dummy_weights(
                 # XLA device does not support torch.Generator()
                 param.uniform_(low, high)
                 continue
-            
+
             if current_platform.is_hpu():
                 import habana_frameworks.torch.hpu.random as htrandom
                 generator = htrandom.default_generators[0]
             else:
                 generator = torch.Generator(device=param.data.device)
-                
+
             generator.manual_seed(seed)
             if torch.finfo(param.data.dtype).bits < 16:
                 # uniform_ doesn't support < 16-bit datatypes (FP8)
