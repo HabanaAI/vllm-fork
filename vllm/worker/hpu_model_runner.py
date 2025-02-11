99ba0bf583985b0aaf355b04bec0f29fccc7fdf4
Marceli Fylcek <mfylcek@habana.ai>
2025-02-11 13:40:54 +0200

    Free logits during multistep

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -2350,15 +2350,16 @@ class HPUModelRunner(HPUModelRunnerBase[ModelInputForHPUWithSamplingMetadata]):
                 else:
                     should_sample = True
 
-                #! Should Save logits and idx only when not sampling
+                # Should save logits only when not sampling
+                # Keeping logits during MSS causes memory issues
                 if (self.scheduler_config.enable_delayed_sampling
                         and model_input.seq_group_metadata_list is not None
-                        and self.is_driver_worker and not should_sample):
+                        and self.is_driver_worker):
                     for idx, seq_group_metadata in enumerate(model_input.seq_group_metadata_list):
                         assert len(seq_group_metadata.seq_data) == 1
                         for seq_data in seq_group_metadata.seq_data.values():
-                            seq_data.prev_logits = logits
-                            seq_data.prev_logits_idx = idx
+                            seq_data.prev_logits = logits if not should_sample else None
+                            seq_data.prev_logits_idx = idx if not should_sample else None
 
                 htorch.core.mark_step()
                 # Only perform sampling in the driver worker.
