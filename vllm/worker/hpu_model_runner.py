ef838e63cd85911894b1dafc234d7cd90c24d60a
Saehanseul <saehanseulyi@habana.ai>
2025-02-19 20:01:01 +0200

    delayed sampling new request fix

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -2670,9 +2670,8 @@ class HPUModelRunner(HPUModelRunnerBase[ModelInputForHPUWithSamplingMetadata]):
         model_input = self.cached_step_inputs.pop(0)
         delayed_output = self.cached_step_outputs.pop(0).cpu().squeeze(-1).tolist()
         ctx = model_input.async_callback.keywords["ctx"]
-        if len(ctx.output_queue) == 0:
-            self.cached_step_inputs.pop(0)
-        elif len(ctx.output_queue) == 1:
+        #assert len(ctx.output_queue) == 1, 'There should be exactly 1 output waiting!'
+        if len(ctx.output_queue) > 0:
             output_data = ctx.output_queue[0]
             assert len(output_data.outputs) == 1
             for fake_out, real_out in zip(output_data.outputs[0], delayed_output):
