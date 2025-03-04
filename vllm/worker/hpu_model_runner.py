49a203acd6a62a4b7ccaef808a473fea76b225bf
Karol Damaszke <kdamaszke@habana.ai>
2025-03-04 11:52:04 +0200

    Fix assert

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -2713,6 +2713,8 @@ class HPUModelRunner(HPUModelRunnerBase[ModelInputForHPUWithSamplingMetadata]):
         model_input = self.cached_step_inputs.pop(0)
         delayed_output = self.cached_step_outputs.pop(0).cpu().squeeze(-1).tolist()
         ctx = model_input.async_callback.keywords["ctx"]
+        if len(ctx.output_queue) == 0:
+            return
         assert len(ctx.output_queue) == 1, 'There should be exactly 1 output waiting!'
         output_data = ctx.output_queue[0]
         #assert len(output_data.outputs) == 1
