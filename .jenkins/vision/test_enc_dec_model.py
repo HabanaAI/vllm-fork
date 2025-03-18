b80c37ebd0e798fbe61b7c1c286f4b1879bfd7a5
Michał Kuligowski <mkuligowski@habana.ai>
2025-03-18 06:28:03 +0100

    Update test_enc_dec_model.py

--- .jenkins/vision/test_enc_dec_model.py
+++ .jenkins/vision/test_enc_dec_model.py
@@ -48,7 +48,8 @@ def launch_enc_dec_model(config, question):
     }]
     prompt = tokenizer.apply_chat_template(messages,
                                            add_generation_prompt=True,
-                                           tokenize=False)
+                                           tokenize=False,
+                                           seed=100)
     return llm, prompt
 
 
