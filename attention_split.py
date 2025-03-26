45d78c1a901f49a8a604a19226994eb348b7be57
Sayantan Sarkar <sasarkar@habana.ai>
2025-03-26 04:36:34 +0000

    Add bells and whistles to forloop test

--- attention_split.py
+++ attention_split.py
@@ -3,9 +3,36 @@ from einops import rearrange
 import habana_frameworks.torch
 import sys
 import time
+from habana_frameworks.torch.hpu.metrics import metric_global
+import argparse
+import habana_frameworks.torch.core as htcore
+import contextlib
 
+def profile_ctx():
+        activities = [
+            torch.profiler.ProfilerActivity.HPU,
+            torch.profiler.ProfilerActivity.CPU,
+        ]
+        return torch.profiler.profile(
+            activities=activities,
+            schedule=torch.profiler.schedule(
+                wait=0,
+                warmup=0,
+                active=100,
+                repeat=1,
+            ),
+            on_trace_ready=torch.profiler.tensorboard_trace_handler("log_dir"),
+        )
 
-def attention_split(attn_type):
+def get_slice(t, slice_type, start_idx, end_idx):
+    if slice_type == 'original':
+        return t[:, start_idx:end_idx]
+    elif slice_type == 'index_get':
+        return torch.index_select(t, 1, torch.arange(start_idx, end_idx, device=t.device, dtype=torch.int32))
+    else:
+        assert False
+
+def attention_split(attn_type, index_type, warmup):
     batch_size = 1
     seq_len = 6400
     attn_heads = 16
@@ -16,20 +43,29 @@ def attention_split(attn_type):
     k = torch.randn(batch_size, seq_len, attn_heads, head_dim, device=device)
     v = torch.randn(batch_size, seq_len, attn_heads, head_dim, device=device)
 
-    if attn_type == "split":
+    if attn_type == "split_uniform":
         # start , end , step
+        end_pt = 1601 if warmup else seq_len + 1
         cu_seq_lens = torch.arange(0, seq_len + 1, 64, dtype=torch.int32, device=device)
+    elif attn_type == "split_diffres":
+        if warmup:
+            cu_seq_lens = torch.tensor([   0,   64,  128,  192,  256, 272, 288, 304, 320], device=device)
+        else:
+            cu_seq_lens = torch.tensor(list(range(0,5185,64)) + list(range(5200,6401,16)), device=device)
     else:
         cu_seq_lens = torch.tensor([0, seq_len], dtype=torch.int32, device=device)
-
+    htcore.mark_step(sync=True) # make sure the tensors are created and ready on device
+    gc_metric = metric_global("graph_compilation")
+    print(f"-------- graph_compilation before start: ", gc_metric.stats()[0][1])
     outputs = []
     t1 = time.time()
     for i in range(1, len(cu_seq_lens)):
         start_idx = cu_seq_lens[i - 1].item()
         end_idx = cu_seq_lens[i].item()
-        q_i = q[:, start_idx:end_idx]
-        k_i = k[:, start_idx:end_idx]
-        v_i = v[:, start_idx:end_idx]
+
+        q_i = get_slice(q, index_type, start_idx, end_idx)
+        k_i = get_slice(k, index_type, start_idx, end_idx)
+        v_i = get_slice(v, index_type, start_idx, end_idx)
         q_i, k_i, v_i = (rearrange(x, "b s h d -> b h s d") for x in [q_i, k_i, v_i])
 
         if device == "hpu":
@@ -49,13 +85,33 @@ def attention_split(attn_type):
     context_layer = torch.cat(outputs, dim=1)
     # print(context_layer)
     print(f"attn_type: {attn_type}: Time Taken = {(t2 - t1) * 1000:.3f} ms")
+    print(f'cu_seq_lens = {cu_seq_lens}')
+    gc_metric = metric_global("graph_compilation")
+    print(f"-------- graph_compilation before start: ", gc_metric.stats()[0][1])
 
 
 # # Run the test
 def main():
-    attn_type = sys.argv[1] if len(sys.argv) > 1 else ""
-    attention_split(attn_type)
-
+    parser = argparse.ArgumentParser(description="A simple example of argparse")
+    parser.add_argument("-a", "--attn_type", type=str, help="options for cu_seq_lens", choices=['split_uniform', 'split_diffres', 'nosplit'])
+    parser.add_argument("-i", "--index_type", type=str, help="options for indexing", choices=['original', 'index_get'])
+    parser.add_argument("-p", "--profile", action='store_true', help='Profile')
+    parser.add_argument("-w", "--warmup_exact", action='store_true', help='warmup with exact shapes as actual run')
+    args = parser.parse_args()
+    ctx = profile_ctx if args.profile else contextlib.nullcontext
+    attention_split(args.attn_type, args.index_type, (not args.warmup_exact))  ## warmup
+    with ctx() as p:
+        attention_split(args.attn_type, args.index_type, False)
 
 if __name__ == "__main__":
     main()
+
+
+'''
+We see recompilation in both these cases, which is not ideal
+python attention_split.py -a split_diffres -i index_get
+python attention_split.py -a split_diffres -i original
+
+If we ran with exact shapes, ie if we set the True->False in the warmup run, we wouldnt get recompilations as expected, ie:
+python attention_split.py -a split_diffres -i index_get -w
+'''
