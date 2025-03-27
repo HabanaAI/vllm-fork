95d5a0e9ea936175087cd5e09e11c9c231bc9df6
Michał Kuligowski <mkuligowski@habana.ai>
2025-03-27 14:51:27 +0100

    Update llama.py

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -509,11 +509,7 @@ class LlamaForCausalLM(nn.Module, SupportsLoRA, SupportsPP):
         "output": "lm_head",
         "norm": "model.norm"
     }
-
-
-    def __getattr__(self, name):
-        print(name)
-        return object.__getattribute__(self, name)
+  
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
         super().__init__()
         config = vllm_config.model_config.hf_config
