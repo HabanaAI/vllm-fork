0b6c60c8a0926a516a936d16365adf8898f732dc
Bartosz Kowalski <bkowalski@habana.ai>
2025-04-14 15:07:37 +0300

    Fix wrong variable name for fp8 arguments

--- .jenkins/benchmark/run-benchmark.sh
+++ .jenkins/benchmark/run-benchmark.sh
@@ -75,7 +75,7 @@ export TQDM_DISABLE=True
 fp8_args=""
 if [[ $__fp8 == "yes" ]]; then
     export QUANT_CONFIG=/software/users/kpietkun/configs/llama3.1_quant_cofnig.json
-    extra_args="--quantization=inc \
+    fp8_args="--quantization=inc \
         --kv-cache-dtype=fp8_inc \
         --weights-load_device=cpu "
 fi
