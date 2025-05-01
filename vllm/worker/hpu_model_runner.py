b73769e22f387a65145fc3120b2c745f3ad33bd4
Chendi Xue <chendi.xue@intel.com>
2025-05-02 02:02:26 +0300

    Add profile to decode only
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -648,6 +648,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                                if self.model_config is not None else None)
         self.device_config = (self.device_config if self.device_config
                               is not None else DeviceConfig())
+        self.is_prompt = True
         if is_fake_hpu():
             self.device_config.device = torch.device('cpu')
             self.device_config.device_type = 'cpu'
@@ -1663,6 +1664,22 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                                      slot_mapping=slot_mapping,
                                      lora_ids=lora_ids)
 
+    def start_profile(self):
+        import queue
+        if not hasattr(self, 'pt_profiler') or (hasattr(self, 'pt_profoler_started')  and self.pt_profoler_started):
+            return
+        logger.info("=============== Starting profiler ===============")
+        high_level_profiler = self.profiler
+        with high_level_profiler.record_event('internal', 'start_profiler'):
+            # Clean up the queue
+            while True:
+                try:
+                    high_level_profiler.profiling_trace_events.get_nowait()
+                except queue.Empty:
+                    break
+            self.pt_profiler.start()
+            self.pt_profoler_started = True
+
     def prepare_input_tensors(
         self,
         seq_group_metadata_list: List[SequenceGroupMetadata],
@@ -1684,6 +1701,9 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
 
         self.event_start = self.profiler.get_timestamp_us()
         is_prompt = seq_group_metadata_list[0].is_prompt
+        self.is_prompt = is_prompt
+        if not is_prompt:
+            self.start_profile()
         base_event_name = 'prompt' if is_prompt else 'decode'
         self.profiler.start('internal', base_event_name)
 
