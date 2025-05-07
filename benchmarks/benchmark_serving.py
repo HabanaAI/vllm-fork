6b1c0bc60b46b8ea3eeb8a8985b9dd90a89062a1
Artur Fierka <artur.fierka@intel.com>
2025-05-07 12:28:33 +0300

    TMP - add autoincrement for top_k in serving benchmark
    Signed-off-by: Artur Fierka <artur.fierka@intel.com>

--- benchmarks/benchmark_serving.py
+++ benchmarks/benchmark_serving.py
@@ -346,14 +346,15 @@ async def benchmark(
 
     benchmark_start_time = time.perf_counter()
     tasks: list[asyncio.Task] = []
+    k_i = 2
     async for request in get_request(input_requests, request_rate, burstiness):
         extra_body = {
             "top_p": 0.9,
-            "top_k": random.randint(
-                2, 20
-            ),  # Intel sampler: 6127 (7092); V1 sampler: 6312 (7304, 7525)
+            "top_k":
+            k_i,  # Intel sampler: 6127 (7092); V1 sampler: 6312 (7304, 7525)
             "temperature": 1.0,
         }
+        k_i += 1
         prompt, prompt_len, output_len, mm_content = request.prompt, \
             request.prompt_len, request.expected_output_len, \
                 request.multi_modal_data
