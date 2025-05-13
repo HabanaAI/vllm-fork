2b617269da018295f99a6fc8c91dd52d7626dafb
Sayantan Sarkar <sasarkar@habana.ai>
2025-05-13 01:16:17 +0000

    Add spaces

--- vllm/model_executor/models/qwen2_5_vl.py
+++ vllm/model_executor/models/qwen2_5_vl.py
@@ -980,7 +980,7 @@ class Qwen2_5_VisionTransformerStaticShape(Qwen2_5_VisionTransformer):
         assert_msg = (
             "Expect inputs to be 112x112 aligned. "
             "Please align before sending image or use this version "
-            "of transformer that does the resizing/alignment automatically:"
+            "of transformer that does the resizing/alignment automatically: "
             "pip install git+https://github.com/malkomes/transformers.git@ac372cd18f836c41f57cdce46094db00019d4280")
         assert x.shape[0] % 64 == 0, assert_msg
         hidden_states = x.unsqueeze(1)
@@ -1015,8 +1015,8 @@ class Qwen2_5_VisionTransformerStaticShape(Qwen2_5_VisionTransformer):
             f"(14 is patch size for ViT). "
             f"Got pixel_values shape {pixel_values.shape[0]}"
             f"Please install this version of transformer"
-            f"which automatically does the 112 alignment"
-            f"pip install git+https://github.com/malkomes/transformers.git@ac372cd18f836c41f57cdce46094db00019d4280"
+            f"which automatically does the 112 alignment "
+            f"pip install git+https://github.com/malkomes/transformers.git@ac372cd18f836c41f57cdce46094db00019d4280 "
             f"or pass in images that are 112 aligned")
         offset = 0
         results = []
