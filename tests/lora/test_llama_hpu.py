069958e4bc2688c92f4b4beb8db1b9669f8663b4
Himangshu Lahkar <hlahkar@habana.ai>
2025-05-14 07:28:48 +0300

    Update test cases to avoid hang

--- tests/lora/test_llama_hpu.py
+++ tests/lora/test_llama_hpu.py
@@ -77,23 +77,12 @@ def _test_llama_lora(sql_lora_files, tp_size):
 
 
 def test_llama_lora_1x(sql_lora_files):
-    p = Process(target=_test_llama_lora, args=(sql_lora_files, 1))
-    p.start()
-    p.join()
-    assert p.exitcode == 0
+    _test_llama_lora(sql_lora_files, 1)
 
 
 def test_llama_lora_2x(sql_lora_files):
-    # Work-around to resolve stalling issue in multi-card scenario
-    p = Process(target=_test_llama_lora, args=(sql_lora_files, 2))
-    p.start()
-    p.join()
-    assert p.exitcode == 0
+    _test_llama_lora(sql_lora_files, 2)
 
 
 def test_llama_lora_4x(sql_lora_files):
-    # Work-around to resolve stalling issue in multi-card scenario
-    p = Process(target=_test_llama_lora, args=(sql_lora_files, 4))
-    p.start()
-    p.join()
-    assert p.exitcode == 0
+    _test_llama_lora(sql_lora_files, 4)
