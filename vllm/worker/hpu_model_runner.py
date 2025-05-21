b1ff09bb3a9ea6b619a68db4240695dfa101050e
Gustavo Malkomes <gustavo.malkomes@intel.com>
2025-05-21 23:02:10 +0000

    ops

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -2391,7 +2391,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
         logger.info(msg)
 
     def _warmup_multimodal(self, kv_caches):
-        if not self.get_model().is_mrope_model:
+        if not self.is_mrope_model:
             return
         _, max_seq_len = self.bucketing_ctx.get_max_prompt_shape()
         seq_len = max_seq_len
