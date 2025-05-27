8a42c0a2138ae483e2417fba93b9bc425108f9ef
Tomasz Thaddey <tthaddey@habana.ai>
2025-05-27 10:33:06 +0300

    docker vllm: separate params calculation rules from autocalc main code
    Signed-off-by: Tomasz Thaddey <tthaddey@habana.ai>

--- .cd/entrypoint.sh
+++ .cd/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-INPUT_CSV=settings_vllm.csv
 VARS_FILE=server_vars.txt
 INPUT_SH=template_vllm_server.sh
 OUTPUT_SH=vllm_server.sh
@@ -14,7 +13,7 @@ LOG_FILE=$LOG_DIR/$LOG_FILE
 HF_HOME="${HF_HOME:-/root/.cache/huggingface}"
 export HF_HOME
 
-python3 vllm_autocalc.py settings_vllm.csv
+python3 vllm_autocalc.py
 if [[ $? -ne 0 ]]; then
 	echo "Settings Error. Exiting!"
 	exit -1
