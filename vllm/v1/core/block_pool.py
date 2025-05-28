64cc25c7a5c64a78cc0809818a78af39af818517
Jan Kaniecki <jan.kaniecki@intel.com>
2025-05-28 11:00:03 +0200

    Pre-commit fix

--- vllm/v1/core/block_pool.py
+++ vllm/v1/core/block_pool.py
@@ -6,13 +6,13 @@ from typing import Callable, Optional
 from vllm.distributed.kv_events import (AllBlocksCleared, BlockRemoved,
                                         BlockStored, KVCacheEvent)
 from vllm.logger import init_logger
+from vllm.platforms import current_platform
 from vllm.v1.core.kv_cache_utils import (BlockHashType, FreeKVCacheBlockQueue,
                                          KVCacheBlock,
                                          generate_block_hash_extra_keys,
                                          hash_block_tokens)
 from vllm.v1.request import Request
 
-from vllm.platforms import current_platform
 if current_platform.is_hpu():
     from vllm.v1.core.kv_cache_utils import FreeKVCacheBlockQueueHPU
 
@@ -51,7 +51,8 @@ class BlockPool:
         if current_platform.is_hpu():
             self.free_block_queue = FreeKVCacheBlockQueueHPU(self.blocks)
         else:
-            self.free_block_queue = FreeKVCacheBlockQueue(self.blocks)  # type: ignore
+            self.free_block_queue = FreeKVCacheBlockQueue(
+              self.blocks)  # type: ignore
 
         # {block_hash: {block ID: block}}. A cached block is
         # a full block with a block hash that can be used for prefix caching.
