a6fb2037bee1ed237ddea1e7ae6473dd9e8ea905
Tomasz Thaddey <tthaddey@habana.ai>
2025-06-09 16:58:29 +0300

    docker vllm: refactor docker entrypoint
    Signed-off-by: Tomasz Thaddey <tthaddey@habana.ai>

--- .cd/vllm_autocalc.py
+++ .cd/vllm_autocalc.py
@@ -26,15 +26,15 @@ class VarsGenerator:
         self.model_def_settings = pd.read_csv(model_def_settings_path)
         self.file_output_vars = file_output_vars
         self.context = {}
-        self._build_context()
+        self.build_context()
 
-    def _get_device_name(self):
+    def get_device_name(self):
         import habana_frameworks.torch.hpu as hthpu
         os.environ["LOG_LEVEL_ALL"] = "6"
         device_name = hthpu.get_device_name()
         return device_name
 
-    def _get_model_from_csv(self):
+    def get_model_from_csv(self):
         """
         Reads the model settings CSV and 
         returns a dictionary for the selected model.
@@ -48,7 +48,7 @@ class VarsGenerator:
 
         return filtered.iloc[0].to_dict()
 
-    def _build_context(self):
+    def build_context(self):
         """
         Build context dictionary for autocalc rules and server configuration.
         Loads defaults from self.defaults (from settings/defaults.yaml).
@@ -61,11 +61,11 @@ class VarsGenerator:
         self.context['HPU_MEM'] = defaults.get('HPU_MEM', {})
         self.context['DTYPE'] = defaults.get('DTYPE', "bfloat16")
         self.context['DEVICE_NAME'] = defaults.get(
-            'DEVICE_NAME') or self._get_device_name()
-        server_conf = self._get_model_from_csv()
+            'DEVICE_NAME') or self.get_device_name()
+        server_conf = self.get_model_from_csv()
         self.context.update(server_conf)
 
-    def _overwrite_params(self):
+    def overwrite_params(self):
         """
         Overwrite default values with user provided ones before auto_calc.
         Uses the 'user_update_vars' section from self.varlist_conf.
@@ -80,40 +80,29 @@ class VarsGenerator:
                 print(f"Adding or updating {param} to {self.context[param]}")
         return self.context
 
-    def _auto_calc_all(self):
+    def auto_calc_all(self):
         for param, func in PARAM_CALC_FUNCS.items():
             self.context[param] = func(self.context)
 
-    def _write_dict_to_file(self):
+    def return_dict(self):
         """
-        Write only variables listed in 'output_vars' section of 
-        varlist_conf.yaml as export statements to the output file.
+        Returns a dictionary with variables listed in 'output_vars' 
+        from varlist_conf.yaml.
         """
         output_vars = self.varlist_conf.get('output_vars', [])
-        with open(self.file_output_vars, 'w') as file_obj:
-            for key in output_vars:
-                if key in self.context:
-                    file_obj.write(f"export {key}={self.context[key]}\n")
+        return {
+            key: self.context[key]
+            for key in output_vars if key in self.context
+        }
 
-    def run(self):
+    def calculate_variables(self):
         """
         Main execution method.
         """
-        self._overwrite_params()
+        self.overwrite_params()
         try:
-            self._auto_calc_all()
+            self.auto_calc_all()
         except ValueError as e:
             print("Error:", e)
             exit(-1)
-        self._write_dict_to_file()
-
-
-if __name__ == '__main__':
-    # export  MODEL=meta-llama/Llama-3.1-8B-Instruct
-    defaults_path = 'settings/defaults.yaml'
-    varlist_conf_path = 'settings/varlist_conf.yaml'
-    model_def_settings_path = 'settings/settings_vllm.csv'
-    vg = VarsGenerator(defaults_path=defaults_path,
-                       varlist_conf_path=varlist_conf_path,
-                       model_def_settings_path=model_def_settings_path)
-    vg.run()
+        return self.return_dict()
