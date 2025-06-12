986fb6361f65b55fcb2e2b7812a43f6a3a4c7f21
Krzysztof Smusz <ksmusz@habana.ai>
2025-06-12 16:50:26 +0200

    Fixing prompt_logprobs list in delayed sampling (#1415)
    Prepending missing [None] as the first value of prompt_logprobs when
    delayed sampling is enabled to be in sync with regular sampling
    scenario.
    
    Signed-off-by: Krzysztof Smusz <ksmusz@habana.ai>

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -3623,3 +3623,19 @@ class HPUModelRunner(HPUModelRunnerBase[ModelInputForHPUWithSamplingMetadata]):
                 assert len(sg.seq_group.seqs) == 1
                 assert len(real_logprobs) == 1
                 sg.seq_group.first_seq.output_logprobs[-1] = real_logprobs[0]
+
+        # If prompt logprobs are available, we need to patch them
+        # as well.
+        if prompt_logprobs_required and delayed_prompt_logprobs is not None:
+            seq_groups = output_data.scheduler_outputs.scheduled_seq_groups
+            assert len(seq_groups) == len(delayed_prompt_logprobs), \
+                f'''Output data has {len(seq_groups)} seq groups, but prompt
+                logprobs has {len(delayed_prompt_logprobs)} entries!'''
+            for sg, real_logprobs in zip(seq_groups, delayed_prompt_logprobs):
+                if real_logprobs is not None:
+                    # Prepending None just like in vllm.engine.output_processor\
+                    # .single_step.single_step_process_prompt_logprob, but
+                    # hence we are not going through async output processor
+                    # with data from prompt in delayed sampling scenario we
+                    # need to do that manually.
+                    sg.seq_group.prompt_logprobs = [None] + real_logprobs
