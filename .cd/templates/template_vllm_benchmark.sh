3065f91a96406baab691c5a7f9edeb33df96b4dc
Tomasz Thaddey <tthaddey@habana.ai>
2025-06-13 17:34:28 +0300

    docker vllm: add benchmarking mode to docker
    Signed-off-by: Tomasz Thaddey <tthaddey@habana.ai>

---
+++ .cd/templates/template_vllm_benchmark.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#@VARS
+
+## Start server
+python3 /workspace/vllm/benchmarks/benchmark_serving.py \
+                 --model $MODEL \
+                 --base-url http://localhost:8000 \
+                 --backend vllm \
+                 --dataset-name sonnet \
+                 --dataset-path /workspace/vllm/benchmarks/sonnet.txt \
+                 --sonnet-prefix-len 100 \
+                 --sonnet-input-len $INPUT_TOK \
+                 --sonnet-output-len $OUTPUT_TOK \
+                 --ignore-eos \
+                 --trust-remote-code \
+                 --num-prompts $NUM_PROMPTS \
+                 --max-concurrency $CONCURRENT_REQ \
+                 --metric-percentiles 90 \
+2>&1 | tee -a logs/perftest_inp${INPUT_TOK}_out${OUTPUT_TOK}_user${CONCURRENT_REQ}.log
