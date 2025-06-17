d75936dd102904a4cd0dd2611ea97bf7d828a6fe
Bob Zhu <bob.zhu@intel.com>
2025-06-17 11:10:07 +0800

    Update hpu_model_runner.py to fix the off_value nan when with float16

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -397,7 +397,7 @@ class HpuModelAdapter(torch.nn.Module):
             mask = attn_mask.logical_or(len_mask).logical_or(len_mask_v)
             off_value = -3E38  #small number, avoid nan and overflow
             if dtype == torch.float16:
-                off_value = -63000
+                off_value = -63000    # a small value close to float16.min
         else:
             mask = attn_mask.logical_or(
                 len_mask)  #no need for len_mask_v as decode overwrites it
