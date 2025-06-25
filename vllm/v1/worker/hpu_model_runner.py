548491681169dafe117d0f29e6b7edd06f239c8f
Yeonsil Yoon <yeon.sil.yoon@intel.com>
2025-06-25 13:25:25 -0700

    Fix code format

--- vllm/v1/worker/hpu_model_runner.py
+++ vllm/v1/worker/hpu_model_runner.py
@@ -423,7 +423,7 @@ class HpuModelAdapter(torch.nn.Module):
         #        selected_token_indices = kwargs.pop('selected_token_indices')
         if 'warmup_mode' in kwargs:
             kwargs.pop('warmup_mode')
-        is_warmup = kwargs['is_warmup'] if 'is_warmup' in kwargs else False
+        is_warmup = kwargs.get('is_warmup', False)
         input_ids = kwargs['input_ids']
         kwargs['attn_metadata'] = self._update_metadata(
             kwargs['attn_metadata'], input_ids.size(0), input_ids.size(1),
@@ -858,11 +858,8 @@ class HPUModelRunner:
         return self.model
 
     def is_decoder_only(self, req_id) -> bool:
-        if req_id in self.input_batch.req_type and self.input_batch.req_type[
-                req_id] == "decode":
-            return True
-        else:
-            return False
+        return bool(req_id in self.input_batch.req_type and \
+            self.input_batch.req_type[req_id] == "decode")
 
     def _get_prompts_and_decodes(
         self,
@@ -887,16 +884,16 @@ class HPUModelRunner:
             if requests is not None and req_id not in self.input_batch.req_type:
                 for request in requests:
                     if request.req_id == req_id:
-                        self.input_batch.req_type[
-                            req_id] = "prefill" if request.load_spec is None else "decode"
+                        self.input_batch.req_type[req_id] = "prefill" \
+                            if request.load_spec is None else "decode"
                         break
 
             num_computed_tokens = self.input_batch.num_computed_tokens_cpu[i]
             num_prompt_tokens = self.input_batch.num_prompt_tokens[i]
             num_scheduled_tokens = scheduler_output.num_scheduled_tokens[
                 req_id]
-            if num_computed_tokens < num_prompt_tokens and not self.is_decoder_only(
-                    req_id):
+            if num_computed_tokens < num_prompt_tokens and \
+                not self.is_decoder_only(req_id):
                 # This is prompt
                 break
 
