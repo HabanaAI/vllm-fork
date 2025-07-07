e08e1cfc0b3442e5f457b417aaf910328a958a57
Youlei Yang <youlei.yang@intel.com>
2025-07-07 06:53:03 +0000

    restore profie run for multimodal models

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -2296,21 +2296,25 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
         max_batch_size = min(self.max_num_seqs,
                              self.max_num_batched_tokens // max_seq_len)
 
-        # Using batch_size 1 is profile multimodal models
-        max_batch_size = max_batch_size if not self.model_is_mrope else 1
 
         msg = (f"profiling run with {max_batch_size=}, {max_seq_len=}")
         logger.info(msg)
 
-        self.warmup_scenario(
-            batch_size=max_batch_size,
-            seq_len=max_seq_len,
-            is_prompt=True,
-            kv_caches=kv_caches,
-            is_pt_profiler_run=False,
-            num_patches=UNSET_NUM_PATCHES,
-            is_lora_profile_run=True,
-        )
+        if self.model_is_mrope:
+            logger.warning("reset max_batch_size to 1 for multimodal models")
+            max_batch_size = 1
+            self.warmup_scenario(
+                batch_size=max_batch_size,
+                seq_len=max_seq_len,
+                is_prompt=True,
+                kv_caches=kv_caches,
+                is_pt_profiler_run=False,
+                num_patches=UNSET_NUM_PATCHES,
+                is_lora_profile_run=True,
+            )
+        
+        self.warmup_scenario(max_batch_size, max_seq_len, True, kv_caches,
+                             False, True)
 
         return
 
