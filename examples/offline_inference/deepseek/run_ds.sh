9c69743190483bf138b03ae2963406bf7158e8ab
yiliu30 <yi4.liu@intel.com>
2025-07-22 04:47:10 +0000

    update cmd
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- examples/offline_inference/deepseek/run_ds.sh
+++ examples/offline_inference/deepseek/run_ds.sh
@@ -4,14 +4,15 @@ DEFAULT_MODEL_PATH="/mnt/disk3/DeepSeek-R1-G2"
 DEFAULT_MODEL_PATH="/mnt/disk3/yiliu4/DeepSeek-R1-G2-INC-424-Converter207"
 FP8_MODEL_PATH="${1:-$DEFAULT_MODEL_PATH}"
 
-timestamp=$(date +%Y%m%d_%H%M%S)
-LOG_FILE="dynamic_quant.inc.${timestamp}.log"
-
+export CALC_SCALE_WITH_CGUID=1
 export QUANT_CONFIG="./quant_configs/dynamic_quant_config.json"
 export VLLM_SKIP_WARMUP=true
 
 WORLD_SIZE=8
 # WORLD_SIZE=1
+
+timestamp=$(date +%Y%m%d_%H%M%S)
+LOG_FILE="dynamic_quant.inc.${timestamp}.log"
 
 PT_HPU_LAZY_MODE=1 \
     python deepseek_example.py \
