0e80b4d814ea263a22b2aa1057a29ec0f87f2060
Iman Gohari <s.m.iman.gohari@intel.com>
2025-08-05 17:15:48 +0000

    fea(): updated the online tests qauntization

--- hpu_runner.sh
+++ hpu_runner.sh
@@ -1,5 +1,6 @@
 ModelName=Qwen/Qwen2.5-VL-3B-Instruct
-logfile=g2-1x-pr1163-3698d03fb_1.21-555.offline-pytest-bf16online-fp8inc_online-NoCustomTransformers.log
+logfile=g2-1x-122-570.1_22.0_next_a12463ci-online-fp8.log
+logfile=g2-1x-122-570.hpugraphgemma3-62d176ca-online-fp8.log
 
 echo_exe() {
     echo "$1"
@@ -7,7 +8,7 @@ echo_exe() {
     eval "$1" 2>&1 | tee -a "$2"
 }
 
-if true; then
+if false; then
     cmd="bash run_offline.sh -m Qwen/Qwen2.5-VL-3B-Instruct -i --random_image_size --iter 5 --image_width 1120 --image_height 1120"
     echo_exe "$cmd" "$logfile"
     cmd="bash run_offline.sh -m Qwen/Qwen2.5-VL-3B-Instruct -i --random_image_size --iter 5 --image_width 1120 --image_height 1120 --multiple_prompts"
@@ -45,7 +46,7 @@ if true; then
     cmd="VLLM_SKIP_WARMUP=true pytest tests/models/multimodal/generation/test_common.py -s -v -k qwen2_5_vl"
     echo_exe "$cmd" "$logfile"
 fi
-if true; then
+if false; then
     source ~/helpers/env.sh
     set_intel_proxy
     cmd="bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 10"
@@ -61,6 +62,7 @@ if true; then
 
     cmd="bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 500"
     echo_exe "$cmd" "$logfile"
+
     cmd="bash run_online.sh -m $ModelName --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 200"
     echo_exe "$cmd" "$logfile"
     #cmd="bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 100 -tp 2"
@@ -70,14 +72,20 @@ if true; then
     cmd="bash run_online.sh -m $ModelName --skip_warmup --hpu -ds sonnet --num-prompts 1000"
     echo_exe "$cmd" "$logfile"
 
-    cmd="QUANT_CONFIG=/root/vllm-fork/calibration/vllm-hpu-extension/calibration/g2/qwen2.5-vl-3b-instruct/maxabs_quant_g2.json bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 100 --fp8"
+    cmd="bash run_online.sh -m $ModelName --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 200 --save_generated-text"
     echo_exe "$cmd" "$logfile"
-    cmd="QUANT_CONFIG=/root/vllm-fork/calibration/vllm-hpu-extension/calibration/g2/qwen2.5-vl-3b-instruct/maxabs_quant_g2.json bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 200 --fp8"
+fi
+if true; then
+    source ~/helpers/env.sh
+    set_intel_proxy
+    cmd="QUANT_CONFIG=/root/vllm-fork/calibration/vllm-hpu-extension/calibration/inc_g2_1.22.0/qwen2.5-vl-3b-instruct/maxabs_quant_g2.json bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 100 --fp8"
     echo_exe "$cmd" "$logfile"
-    cmd="QUANT_CONFIG=/root/vllm-fork/calibration/vllm-hpu-extension/calibration/g2/qwen2.5-vl-3b-instruct/maxabs_quant_g2.json bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 300 --fp8"
+    cmd="QUANT_CONFIG=/root/vllm-fork/calibration/vllm-hpu-extension/calibration/inc_g2_1.22.0/qwen2.5-vl-3b-instruct/maxabs_quant_g2.json bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 200 --fp8"
     echo_exe "$cmd" "$logfile"
-    cmd="QUANT_CONFIG=/root/vllm-fork/calibration/vllm-hpu-extension/calibration/g2/qwen2.5-vl-3b-instruct/maxabs_quant_g2.json bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 400 --fp8"
+    cmd="QUANT_CONFIG=/root/vllm-fork/calibration/vllm-hpu-extension/calibration/inc_g2_1.22.0/qwen2.5-vl-3b-instruct/maxabs_quant_g2.json bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 300 --fp8"
     echo_exe "$cmd" "$logfile"
-    cmd="QUANT_CONFIG=/root/vllm-fork/calibration/vllm-hpu-extension/calibration/g2/qwen2.5-vl-3b-instruct/maxabs_quant_g2.json bash run_online.sh -m $ModelName --skip_warmup --hpu -ds sonnet --num-prompts 1000 --fp8"
+    cmd="QUANT_CONFIG=/root/vllm-fork/calibration/vllm-hpu-extension/calibration/inc_g2_1.22.0/qwen2.5-vl-3b-instruct/maxabs_quant_g2.json bash run_online.sh -m $ModelName --skip_warmup --hpu -ds lmarena-ai/vision-arena-bench-v0.1 --num-prompts 400 --fp8"
+    echo_exe "$cmd" "$logfile"
+    cmd="QUANT_CONFIG=/root/vllm-fork/calibration/vllm-hpu-extension/calibration/inc_g2_1.22.0/qwen2.5-vl-3b-instruct/maxabs_quant_g2.json bash run_online.sh -m $ModelName --skip_warmup --hpu -ds sonnet --num-prompts 1000 --fp8"
     echo_exe "$cmd" "$logfile"
 fi
