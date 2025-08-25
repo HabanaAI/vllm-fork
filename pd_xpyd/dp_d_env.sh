3c8c732f0a3bedb551732341e052eeb2961be7ed
Tony Lin <tony.lin@intel.com>
2025-08-25 12:20:45 +0000

    enable inf fp8 in PD-DP

--- pd_xpyd/dp_d_env.sh
+++ pd_xpyd/dp_d_env.sh
@@ -5,22 +5,20 @@ source "$BASH_DIR"/pd_env.sh
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
-model_path=/mnt/disk2/hf_models/DeepSeek-R1-BF16-w8afp8-static-no-ste-G2/
-
-export VLLM_GPU_MEMORY_UTILIZATION=0.9
-export VLLM_GRAPH_RESERVED_MEM=0.2
+export VLLM_GPU_MEMORY_UTILIZATION=0.7
+export VLLM_GRAPH_RESERVED_MEM=0.3
 export VLLM_GRAPH_PROMPT_RATIO=0
 
 # enable delayed samping on decode
 export VLLM_DELAYED_SAMPLING="true"
 
 # params
-model_len=8192
-max_num_batched_tokens=8192
-max_num_seqs=32
+model_len=16384
+max_num_batched_tokens=16384
+max_num_seqs=64
 input_min=128
-input_max=8192
-output_max=8192
+input_max=16384
+output_max=16384
 
 unset VLLM_PROMPT_BS_BUCKET_MIN VLLM_PROMPT_BS_BUCKET_STEP VLLM_PROMPT_BS_BUCKET_MAX
 unset VLLM_PROMPT_SEQ_BUCKET_MIN VLLM_PROMPT_SEQ_BUCKET_STEP VLLM_PROMPT_SEQ_BUCKET_MAX
@@ -37,8 +35,8 @@ export VLLM_PROMPT_SEQ_BUCKET_STEP=128
 export VLLM_PROMPT_SEQ_BUCKET_MAX=1
 
 #export VLLM_DECODE_BLOCK_BUCKET_MIN=2048
-export VLLM_DECODE_BS_BUCKET_STEP=1
-export VLLM_DECODE_BLOCK_BUCKET_STEP=2
+export VLLM_DECODE_BS_BUCKET_STEP=2
+#export VLLM_DECODE_BLOCK_BUCKET_STEP=2
 
 echo " environments are reseted "
 
@@ -49,6 +47,7 @@ env | grep VLLM_DECODE_BLOCK
 
 export VLLM_SKIP_WARMUP=True
 #unset VLLM_SKIP_WARMUP
+#export PT_HPU_RECIPE_CACHE_CONFIG=/workspace/ww33_inc_fp8_d,false,131072
 
 export VLLM_DP_SIZE=2
 export VLLM_USE_V1=0
@@ -56,3 +55,8 @@ export VLLM_DP_MASTER_IP=10.239.129.81
 export VLLM_DP_MASTER_PORT=25940
 export VLLM_EP_SIZE=16
 
+export PT_HPU_MOE_THRESHOLD=64
+
+if [ "$INC_FP8" -eq 1 ]; then
+  export QUANT_CONFIG="$BASH_DIR"/inc_fp8_tp1ep16.json
+fi
--- pd_xpyd/dp_start_decode.sh
+++ pd_xpyd/dp_start_decode.sh
@@ -5,6 +5,10 @@
 BASH_DIR=$(dirname "${BASH_SOURCE[0]}")
 source "$BASH_DIR"/dp_d_env.sh
 
+timestamp=$(date +"%Y%m%d_%H%M%S")
+log_dir="xpyd_logs"
+mkdir -p "$log_dir"
+
 export MOONCAKE_CONFIG_PATH="$BASH_DIR"/mooncake_$1.json
 echo "MOONCAKE_CONFIG_PATH=$MOONCAKE_CONFIG_PATH"
 
@@ -36,6 +40,14 @@ if [ "$DP_SIZE" -eq 1 ]; then
   unset VLLM_DP_MASTER_PORT
 fi
 
+if [ "$INC_FP8" -eq 1 ]; then
+  kv_cache_dtype_arg="--kv-cache-dtype fp8_inc"
+  echo "<decode>it's inc fp8 kv cache mode"
+else
+  kv_cache_dtype_arg=""
+  echo "<decode>it's bf16 kv cache mode"
+fi
+
 for ((i=0; i<$DP_RANK; i++))
 do
   RANK=$((DP_INDEX * DP_RANK + i))
@@ -55,12 +67,19 @@ do
     --use-padding-aware-scheduling
     --use-v2-block-manager
     --distributed_executor_backend mp
+    $kv_cache_dtype_arg
     --kv-transfer-config '{"kv_connector":"MooncakeStoreConnector","kv_role":"kv_consumer"}'
   )
+  log_file="$log_dir/log_rank${RANK}_${timestamp}.log"
+
+  extra_env=()
+#  if [ "$i" -eq 0 ] && [ "$RANK" -eq 0 ]; then
+#    extra_env+=(VLLM_PROFILER_ENABLED=true)
+#  fi
 
   if [ "$DP_RANK" -ne 1 ]; then
     echo "env VLLM_DP_RANK=$RANK ${CMD[*]}"
-    env VLLM_DP_RANK_LOCAL="$i" VLLM_DP_RANK="$RANK" "${CMD[@]}" &
+    env VLLM_DP_RANK_LOCAL="$i" VLLM_DP_RANK="$RANK" "${extra_env[@]}" "${CMD[@]}" 2>&1 | tee "$log_file" &
   else
     echo "${CMD[*]}"
     "${CMD[@]}" &
