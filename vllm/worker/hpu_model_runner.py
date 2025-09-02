27f0aa7bcae380be9f43d46d1c9b62d33bff4af3
Chen, Wenbin <wenbin.chen@intel.com>
2025-09-02 06:27:17 +0000

    Enable Qwen2_5Omni audio processing
    example:
    PT_HPU_LAZY_MODE=1 python examples/offline_inference/audio_language.py \
      --model-type qwen2_5_omni
    
    Signed-off-by: Chen, Wenbin <wenbin.chen@intel.com>

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -369,6 +369,10 @@ class HpuModelAdapter(torch.nn.Module):
                 logger.info("[Multimodal] Wrapping Visual Model")
                 self.model.visual = htorch.hpu.wrap_in_hpu_graph(
                     self.model.visual, disable_tensor_cache=True)
+            if self.model_is_mrope and hasattr(self.model, 'audio_tower'):
+                logger.info("[Multimodal] Wrapping Audio Model")
+                self.model.audio_tower = htorch.hpu.wrap_in_hpu_graph(
+                    self.model.audio_tower)
 
             if self.is_mm_optimized:
                 if hasattr(self.model, 'vision_tower'):
@@ -1431,10 +1435,13 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
     def _get_mrope_positions_and_delta(self, seq_data, mm_kwargs, context_len):
         image_grid_thw = mm_kwargs.get("image_grid_thw", None)
         video_grid_thw = mm_kwargs.get("video_grid_thw", None)
+        audio_feature_lengths = mm_kwargs.get("audio_feature_lengths", None)
         second_per_grid_ts = mm_kwargs.get("second_per_grid_ts", None)
-        assert image_grid_thw is not None or video_grid_thw is not None, (
+        assert image_grid_thw is not None or video_grid_thw is not None or \
+          audio_feature_lengths is not None, (
             "mrope embedding type requires multi-modal input mapper "
-            "returns 'image_grid_thw' or 'video_grid_thw'.")
+            "returns 'image_grid_thw' or 'video_grid_thw' or "
+            "audio_feature_lengths.")
         hf_config = self.model_config.hf_config
         token_ids = seq_data.get_token_ids()
         mrope_positions, mrope_position_delta = \
@@ -1445,6 +1452,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                 video_grid_thw=video_grid_thw,
                 second_per_grid_ts=second_per_grid_ts,
                 context_len=context_len,
+                audio_feature_lengths=audio_feature_lengths,
             )
         assert mrope_positions is not None
         return mrope_positions, mrope_position_delta
