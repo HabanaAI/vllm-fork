78f8d14e9f5478bf5b4990c0e563e72f5620ebc6
Libin Tang <litang@habana.ai>
2025-09-05 15:37:18 -0700

    revert unneeded change

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -379,7 +379,7 @@ class HpuModelAdapter(torch.nn.Module):
             if self.is_mm_optimized:
                 if hasattr(self.model, 'vision_tower'):
                     self.model.vision_tower = htorch.hpu.wrap_in_hpu_graph(
-                        self.model.vision_tower, disable_tensor_cache=False)
+                        self.model.vision_tower, disable_tensor_cache=True)
                 if hasattr(self.model, 'multi_modal_projector'):
                     self.model.multi_modal_projector = \
                             htorch.hpu.wrap_in_hpu_graph( \
