3793d04d6f557c453b8f271400ebecf4222ec5a9
tianyuan211 <paulyuen@umich.edu>
2025-09-10 17:20:57 +0800

    Revert "Update ovis2_5.py"
    This reverts commit 87a5b34391068815ac9b2ee5e97e357af7e3e64c.

--- vllm/model_executor/models/ovis2_5.py
+++ vllm/model_executor/models/ovis2_5.py
@@ -431,7 +431,7 @@ class Ovis2_5(nn.Module, SupportsMultiModal, SupportsPP):
         # 让 vLLM 能拿到“文本侧”配置（Qwen3）, 一些代码会直接读 .text_config
         if not hasattr(config, "text_config"):
             config.text_config = config.llm_config
-        # vllm_config.model_config.hf_text_config = config.get_text_config()
+        vllm_config.model_config.hf_text_config = config.get_text_config()
 
         self.config: PretrainedConfig = config
         self.llm = init_vllm_registered_model(
