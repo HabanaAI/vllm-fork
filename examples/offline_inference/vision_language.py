195114328b277c7f95d244b1da2f3af50fca139f
tianyuan211 <paulyuen@umich.edu>
2025-09-19 13:32:40 +0800

    Update vision_language.py

--- examples/offline_inference/vision_language.py
+++ examples/offline_inference/vision_language.py
@@ -1043,7 +1043,7 @@ def run_qwen2_5_vl(questions: list[str], modality: str) -> ModelRequestData:
             "fps": 1,
         },
         limit_mm_per_prompt={modality: 1},
-        tensor_parallel_size=4,
+        tensor_parallel_size=2,
     )
 
     if modality == "image":
