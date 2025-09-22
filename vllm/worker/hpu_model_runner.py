HabanaAI/vllm-fork

commit 1b6b8eb3f35abcfec8f671c18d0510a3f72384c2

disable HPUgraph for prefills with batched_tokens>max_seq_len_to_capture (#1955)
Signed-off-by: Youlei Yang <youlei.yang@intel.com>

vllm/worker/hpu_model_runner.py
@@ -1055,6 +1055,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
         self.max_model_len = self.scheduler_config.max_model_len
         self.max_num_batched_tokens = \
             self.scheduler_config.max_num_batched_tokens
+        self.max_seq_len_to_capture = self.model_config.max_seq_len_to_capture
         self.block_size = self.cache_config.block_size
         self.use_merged_prefill = get_config().merged_prefill
         assert not (self.scheduler_config.use_padding_aware_scheduling
@@ -1485,8 +1486,10 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             return self.model.model
         return self.model
 
-    def _use_graphs(self):
-        return not self.enforce_eager
+    def _use_graphs(self, batch_size, seq_len):
+        if self.enforce_eager:
+            return False
+        return batch_size * seq_len <= self.max_seq_len_to_capture
 
     def _is_valid_bucket(self, bucket):
         return bucket[0] * bucket[1] <= self.max_num_batched_tokens
@@ -2983,7 +2986,7 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                         align_worker=False,
                         is_dummy_run=False) -> None:
         phase = 'prompt' if is_prompt else 'decode'
-        use_graphs = is_dummy_run or self._use_graphs()
+        use_graphs = is_dummy_run or self._use_graphs(batch_size, seq_len)
 
         scenario_name = ("warmup_"
                          f"{phase}_"
@@ -3870,7 +3873,7 @@ class HPUModelRunner(HPUModelRunnerBase[ModelInputForHPUWithSamplingMetadata]):
                 if not warmup_mode:
                     ctx_blocks = seq_len
                 seq_len = 1
-            use_graphs = self._use_graphs()
+            use_graphs = self._use_graphs(batch_size_padded, seq_len)
             self._check_config(batch_size, seq_len, ctx_blocks, attn_metadata,
                                warmup_mode)
             lora_mask: torch.Tensor = None

scripts/utils.sh
@@ -271,7 +271,7 @@ set_linear_bucketing(){
 
     prompt_bs_step=1
     prompt_bs_min=1
-    prompt_bs_max=$(( $max_num_batched_tokens / $(ceil $input_min $block_size) ))
+    prompt_bs_max=$(( $PREFERED_SEQ_LEN_TO_CAPTURE / $(ceil $input_min $block_size) ))
     prompt_bs_max=$( ceil $prompt_bs_max $prompt_bs_step )
     prompt_bs_max=$( min $prompt_bs_max $max_num_seqs )
     if [ -n "$max_num_prefill_seqs" ]; then

commit 253db90ee41110a0e65635c3b86d2e806b7faa6b

fix batch_size_padded=None error introduced by #1955
Signed-off-by: Youlei Yang <youlei.yang@intel.com>

vllm/worker/hpu_model_runner.py
@@ -3873,7 +3873,7 @@ class HPUModelRunner(HPUModelRunnerBase[ModelInputForHPUWithSamplingMetadata]):
                 if not warmup_mode:
                     ctx_blocks = seq_len
                 seq_len = 1
-            use_graphs = self._use_graphs(batch_size_padded, seq_len)
+            use_graphs = self._use_graphs(batch_size, seq_len)
             self._check_config(batch_size, seq_len, ctx_blocks, attn_metadata,
                                warmup_mode)
             lora_mask: torch.Tensor = None