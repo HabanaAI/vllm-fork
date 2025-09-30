7946bde22774e803321eba4ad83738af5a0c3520
Luca Calabria <luca.calabria@intel.com>
2025-09-30 15:35:15 +0200

    added server and client scripts

---
+++ llama4-scripts/vllm-client-fp8-maverick.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+model="meta-llama/Llama-4-Maverick-17B-128E-Instruct"
+tokenizer="meta-llama/Llama-4-Maverick-17B-128E-Instruct"
+model_name="Maverick"
+ 
+mkdir -p benchmark_logs
+
+########################################################## Concurrency 16 Sonnet #################################################################
+max_concurrency_client=32
+num_prompts=32
+request_rate=inf
+in_len=122880
+out_len=8192
+start_time=$(date +%s)
+echo "Start to benchmark"
+python3 ../benchmarks/benchmark_serving.py \
+    --backend vllm \
+    --model ${model} \
+    --tokenizer ${tokenizer} \
+    --dataset-name random \
+    --request-rate ${request_rate} \
+    --percentile-metrics ttft,tpot,itl,e2el \
+	--metric-percentiles 50,90,99 \
+    --ignore-eos \
+    --num-prompts ${num_prompts} \
+    --port 18080 \
+    --random-input-len ${in_len} \
+    --random-output-len ${out_len} \
+    --max-concurrency ${max_concurrency_client} \
+    --save-result 2>&1 | tee benchmark_logs/g3-${model_name}-in${in_len}-out${out_len}-req${request_rate}-num_prompts${num_prompts}-concurrency${max_concurrency_client}.log
+end_time=$(date +%s)
+echo "Time elapsed: $((end_time - start_time))s"
+sleep 10
---
+++ llama4-scripts/vllm-server-fp8-maverick.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+tp_parrallel=8
+in_len=122880
+out_len=8192
+multi_step=1
+total_len=$((in_len + out_len))
+# if total_len is not multiple of 128, round up to the next multiple of 128
+if [ $((total_len % 128)) -ne 0 ]; then
+    echo 'round up for 128'
+    total_len=$(((total_len / 128 +  1) * 128 ))
+fi
+ep_size=16
+moe_n_slice=1
+gpu_utils=0.95
+bs=32
+block_size=256
+block_step=256
+ 
+log_name="[inc-staticquant-scalar-fp8matmul-split]online-gaudi3-${gpu_utils}util-TPparallel${tp_parrallel}-EP${ep_size}-loop${moe_n_slice}moegroups-multistep${multi_step}_nprompt${num_prompts}_rrate${request_rate}_bs${bs}_i${in_len}_o${out_len}_mdllen${total_len}"
+ 
+prompt_bs_max=1
+# Increase prompt max seq len bucket just in case there are more tokens than provided due to tokenizer
+prompt_seq_max=$((in_len + $block_size))
+total_len_aligned=$(((total_len + block_size - 1) / block_size * block_size))
+VLLM_DECODE_BLOCK_BUCKET_MIN=$((in_len * bs / block_size))
+VLLM_DECODE_BLOCK_BUCKET_MAX=$((total_len_aligned * bs / block_size + block_step))
+ 
+model="meta-llama/Llama-4-Maverick-17B-128E-Instruct"
+tokenizer="meta-llama/Llama-4-Maverick-17B-128E-Instruct"
+model_name="Maverick"
+ 
+mkdir -p benchmark_logs
+
+QUANT_CONFIG="./vllm-hpu-extension-quant/llama-4-maverick-17b-128e-instruct/maxabs_quant_g3.json" \
+VLLM_PROMPT_BS_BUCKET_MIN=1 \
+VLLM_PROMPT_BS_BUCKET_MAX=$prompt_bs_max \
+VLLM_PROMPT_SEQ_BUCKET_MIN=${in_len} \
+VLLM_PROMPT_SEQ_BUCKET_MAX=${prompt_seq_max} \
+VLLM_DECODE_BS_BUCKET_MIN=${bs} \
+VLLM_DECODE_BS_BUCKET_MAX=${bs} \
+VLLM_DECODE_BLOCK_BUCKET_MIN=${VLLM_DECODE_BLOCK_BUCKET_MIN} \
+VLLM_DECODE_BLOCK_BUCKET_MAX=${VLLM_DECODE_BLOCK_BUCKET_MAX} \
+VLLM_DECODE_BLOCK_BUCKET_STEP=$block_step \
+VLLM_DELAYED_SAMPLING=true \
+HABANA_VISIBLE_DEVICES="ALL" \
+VLLM_EP_SIZE=${ep_size} \
+PT_HPU_LAZY_MODE=1 \
+PT_HPU_ENABLE_LAZY_COLLECTIVES=true \
+PT_HPU_WEIGHT_SHARING=0 \
+VLLM_EXPONENTIAL_BUCKETING=false \
+vllm serve ${model} \
+    --port 18080 \
+    --tensor-parallel-size ${tp_parrallel} \
+    --max-num-seqs ${bs} \
+    --disable-log-requests \
+    --dtype bfloat16 \
+    --num_scheduler_steps ${multi_step} \
+    --max-model-len ${total_len} \
+    --max-num-batched-tokens 131072 \
+    --use-padding-aware-scheduling \
+    --block-size ${block_size} \
+    --weights-load-device cpu \
+    --use-v2-block-manager \
+    --kv-cache-dtype fp8_inc \
+    --gpu_memory_utilization ${gpu_utils} \
+    --quantization inc \
+    --enable-expert-parallel \
+    --override-generation-config='{"attn_temperature_tuning": true}' \
+    --trust_remote_code 2>&1 | tee benchmark_logs/${log_name}_serving.log &
+pid=$(($!-1))
+ 
+until [[ "$n" -ge 1000 ]] || [[ $ready == true ]]; do
+    n=$((n+1))
+    if grep -q "Started server process" benchmark_logs/${log_name}_serving.log; then
+        break
+    fi
+    sleep 5s
+done
+echo ${pid}
