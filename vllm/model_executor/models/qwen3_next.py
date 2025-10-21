67efaa957d2d34a3cc4f21d28e9ddf60b181f33e
Jing <jing1.ling@intel.com>
2025-10-21 17:36:49 +0800

    fix format
    Signed-off-by: Jing <jing1.ling@intel.com>

--- vllm/model_executor/models/qwen3_next.py
+++ vllm/model_executor/models/qwen3_next.py
@@ -129,9 +129,9 @@ def torch_chunk_gated_delta_rule(
     attn = attn + torch.eye(chunk_size, dtype=attn.dtype, device=attn.device)
     value = attn @ v_beta
     k_cumdecay = attn @ (k_beta * g.exp().unsqueeze(-1))
-    last_recurrent_state = (torch.zeros(batch_size, num_heads,
-                                        k_head_dim, v_head_dim).to(value) if
-                            initial_state is None else initial_state.to(value))
+    last_recurrent_state = (torch.zeros(batch_size, num_heads, k_head_dim,
+                                        v_head_dim).to(value) if initial_state
+                            is None else initial_state.to(value))
     core_attn_out = torch.zeros_like(value)
     mask = torch.triu(torch.ones(chunk_size,
                                  chunk_size,
