7b9daa02454de6b7a81c8b2109e2d22e45753582
Chen, Wenbin <wenbin.chen@intel.com>
2025-10-28 04:09:50 +0000

    Add max_pixels option.
    Add max_pixels option, so that vllm resize large images.
    
    Signed-off-by: Chen, Wenbin <wenbin.chen@intel.com>

--- scripts/start_gaudi_vllm_server.sh
+++ scripts/start_gaudi_vllm_server.sh
@@ -189,6 +189,7 @@ python3 -m vllm.entrypoints.openai.api_server \
     --trust-remote-code \
     --seed 2025 \
     --distributed_executor_backend "${dist_backend}" \
+    --mm_processor_kwargs "max_pixels=1003520" \
     "${extra_params[@]}")
 
 echo "Start a vLLM server for ${model_name} on Gaudi $DEVICE_NAME with command:" |& tee -a "${log_file}"
