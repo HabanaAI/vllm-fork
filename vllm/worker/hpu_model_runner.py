HabanaAI/vllm-fork

commit 17c088b74862f28203f05162f1916781a46e0697

fix OOM for large bf16 weights with INC (#2075)
For large bf16 models like Qwen3-235B-A22B, the original bfloat16
weights cannot fit into 4 Gaudi2 HPUs. But the model could run with INC
fp8 on 4 HPUs by loading to host memory first and covert with INC
layer-by-layer to fp8. This PR skip moving the bf16 weights before the
conversion of INC to avoid OOM.

---------

Signed-off-by: Youlei Yang <youlei.yang@intel.com>

vllm/worker/hpu_model_runner.py
@@ -1359,10 +1359,15 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                 torch.hpu.synchronize()
                 return model
 
-            with HabanaMemoryProfiler() as m_to_hpu:
-                self.model = move_model_to_hpu(self.model)
-            logger.info("Moving model to HPU took %s",
-                        m_to_hpu.get_summary_string())
+            # Move quantized model and tensors to HPU to avoid deduplicated
+            # copies of MoE weights in w13/w2 weights and w13/12 list.
+            # Skip for unquantized model as INC will handle it layer-by-layer.
+            if self.model_config.quantization != 'inc' \
+                and self._is_quant_with_inc():
+                with HabanaMemoryProfiler() as m_to_hpu:
+                    self.model = move_model_to_hpu(self.model)
+                logger.info("Moving model to HPU took %s",
+                            m_to_hpu.get_summary_string())
 
             if self._is_quant_with_inc():
                 logger.info("Preparing model with INC..")

commit 5de37ba1577670142902e0b76ce812d1926fa2a9

Move only the quantized model and tensors to HPU (#2091)
- The non HPU tensor error for bf16 qwen3 models introduced in
https://github.com/HabanaAI/vllm-fork/pull/2075 is fixed by
https://github.com/HabanaAI/vllm-fork/pull/2090
- This PR moves the quantized model and tensors to HPU only to avoid OOM
when the bf16 weights is loaded to CPU then quantized to fp8 with INC.

---------

Signed-off-by: Youlei Yang <youlei.yang@intel.com>

vllm/worker/hpu_model_runner.py
@@ -1355,10 +1355,15 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                 torch.hpu.synchronize()
                 return model
 
-            with HabanaMemoryProfiler() as m_to_hpu:
-                self.model = move_model_to_hpu(self.model)
-            logger.info("Moving model to HPU took %s",
-                        m_to_hpu.get_summary_string())
+            # Move quantized model and tensors to HPU to avoid deduplicated
+            # copies of MoE weights in w13/w2 weights and w13/12 list.
+            # Skip for unquantized model as INC will handle it layer-by-layer.
+            if self.model_config.quantization is not None \
+                and self.model_config.quantization != 'inc':
+                with HabanaMemoryProfiler() as m_to_hpu:
+                    self.model = move_model_to_hpu(self.model)
+                logger.info("Moving model to HPU took %s",
+                            m_to_hpu.get_summary_string())
 
             if self._is_quant_with_inc():
                 logger.info("Preparing model with INC..")