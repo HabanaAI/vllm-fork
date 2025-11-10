9aac9a4e0b74ede497623e4924652f1c91c5a782
tianyuan211 <paulyuen@umich.edu>
2025-11-10 19:52:23 +0800

    update
    Signed-off-by: tianyuan211 <paulyuen@umich.edu>

--- vllm/worker/hpu_model_runner.py
+++ vllm/worker/hpu_model_runner.py
@@ -1396,9 +1396,6 @@ class HPUModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
                                 changed = True
                             new_dict[k] = v
                         if changed:
-                            # skip read-only attributes
-                            if attr_name == "dummy_inputs":
-                                continue
                             setattr(module, attr_name, new_dict)
 
             def move_model_to_hpu(model):
