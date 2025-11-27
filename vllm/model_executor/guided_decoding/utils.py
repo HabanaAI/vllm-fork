49577e92b6757eed23944457c9c3ac83643defc4
12010486 <silvia.colabrese@intel.com>
2025-11-27 13:52:16 +0000

    Fix xgrammar toolcalling v0

--- vllm/model_executor/guided_decoding/utils.py
+++ vllm/model_executor/guided_decoding/utils.py
@@ -17,14 +17,12 @@ def has_xgrammar_unsupported_json_features(schema: dict) -> bool:
 
         # Check for array unsupported keywords
         if obj.get("type") == "array" and any(key in obj for key in [
-                "uniqueItems", "contains", "minContains", "maxContains",
-                "minItems", "maxItems"
+                "uniqueItems", "contains", "minContains", "maxContains"
         ]):
             return True
 
         # Unsupported keywords for strings
-        if obj.get("type") == "string" and any(
-                key in obj for key in ["minLength", "maxLength", "format"]):
+        if obj.get("type") == "string" and "format" in obj:
             return True
 
         # Unsupported keywords for objects
