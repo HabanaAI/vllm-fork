d5418b686975443af5eb4af5524e6a5333210325
Michał Kuligowski <mkuligowski@habana.ai>
2025-12-17 09:41:35 +0100

    Update cli_args.py

--- vllm/entrypoints/openai/cli_args.py
+++ vllm/entrypoints/openai/cli_args.py
@@ -218,6 +218,12 @@ def make_arg_parser(parser: FlexibleArgumentParser) -> FlexibleArgumentParser:
         help="If specified, API server will add X-Request-Id header to "
         "responses. Caution: this hurts performance at high QPS.")
     parser.add_argument(
+        "--h11_max_incomplete_event_size",
+        action="store_true",
+        default=4194304,
+        help="Maximum size (bytes) of an incomplete HTTP event (header or body) for
+    h11 parser. Helps mitigate header abuse. Default: 4194304 (4 MB).")
+    parser.add_argument(
         "--enable-auto-tool-choice",
         action="store_true",
         default=False,
