9a37fa8a815307686a503e591cd606fe91e2e8a7
Zhang, Yanli L <yanli.l.zhang@intel.com>
2025-12-18 17:42:49 +0800

    Add FA3 opt in INC and upgrade mooncake to 0.3.7
    Signed-off-by: Zhang, Yanli L <yanli.l.zhang@intel.com>

--- pd_xpyd/dp_p_env.sh
+++ pd_xpyd/dp_p_env.sh
@@ -95,6 +95,14 @@ env | grep VLLM_DECODE_BS
 env | grep VLLM_DECODE_BLOCK
 # ***************************************  bucketing ends ************************************* #
 
+# FA3 Env Settings
+export PT_HPU_SDPA_QKV_SLICE_MODE_FWD=1
+export PT_HPU_QKV_SLICE_SEQ_LEN_THLD=4096
+export PT_HPU_SDPA_BR_FACTOR=4096       # slice size on the query
+export PT_HPU_SDPA_BC_FACTOR=4096       # siice size on the kv
+export VLLM_FUSEDSDPA_QKV_SLICE_CHUNK_SIZE=4096 # qkv slice size in fp8 FSDPA
+export PT_HPU_QKV_SLICE_IMPL="slice_qkv"    # select the fp8 fsdpa impl
+
 # prefill specific setting
 export VLLM_SKIP_PREFILL_SAMPLING=1
 export VLLM_DP_SIZE=1
@@ -113,6 +121,10 @@ fi
 export VLLM_SUPPORT_MOE_CHUNK="false"  # Can be true after following para are tuned.
 #export PT_HPU_MOE_CHUNK="64, 128"
 #export PT_HPU_MOE_TOKEN_BOUNDARY="2048, 4096"
+
+# MoE Slice Optimization
+#export VLLM_SUPPORT_MOE_SLICE=True
+#export VLLM_MOE_SLICE_LENGTH=8192
 
 # INC FP8 settings
 if [ "$INC_FP8" -eq 1 ]; then
