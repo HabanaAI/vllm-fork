00e78389d01813daaddb85757f7888a54743b118
Zhang, Yanli L <yanli.l.zhang@intel.com>
2025-12-22 22:57:09 +0800

    Update FA3 QKV slice env var name
    Signed-off-by: Zhang, Yanli L <yanli.l.zhang@intel.com>

--- pd_xpyd/dp_p_env.sh
+++ pd_xpyd/dp_p_env.sh
@@ -95,14 +95,6 @@ env | grep VLLM_DECODE_BS
 env | grep VLLM_DECODE_BLOCK
 # ***************************************  bucketing ends ************************************* #
 
-# FA3 Env Settings
-export PT_HPU_SDPA_QKV_SLICE_MODE_FWD=1
-export PT_HPU_QKV_SLICE_SEQ_LEN_THLD=4096
-export PT_HPU_SDPA_BR_FACTOR=4096       # slice size on the query
-export PT_HPU_SDPA_BC_FACTOR=4096       # siice size on the kv
-export VLLM_FUSEDSDPA_QKV_SLICE_CHUNK_SIZE=4096 # qkv slice size in fp8 FSDPA
-export PT_HPU_QKV_SLICE_IMPL="slice_qkv"    # select the fp8 fsdpa impl
-
 # prefill specific setting
 export VLLM_SKIP_PREFILL_SAMPLING=1
 export VLLM_DP_SIZE=1
@@ -125,4 +117,16 @@ export VLLM_SUPPORT_MOE_CHUNK="false"  # Can be true after following para are tu
 # INC FP8 settings
 if [ "$INC_FP8" -eq 1 ]; then
   export QUANT_CONFIG="$BASH_DIR"/inc_fp8_tp8ep8.json
+
+  # FA3 Env Settings - Added for fp8 inc only, not validated for static quant + bf16 kv cache
+  export PT_HPU_SDPA_QKV_SLICE_MODE_FWD=1
+  export VLLM_HPU_FSDPA_SLICE_SEQ_LEN_THLD=4096
+  export PT_HPU_SDPA_BR_FACTOR=4096       # slice size on the query
+  export PT_HPU_SDPA_BC_FACTOR=4096       # siice size on the kv
+  export VLLM_HPU_FSDPA_SLICE_CHUNK_SIZE=4096 # qkv slice size in fp8 FSDPA
+  export VLLM_HPU_FSDPA_SLICE_IMPL="slice_qkv"    # select the fp8 fsdpa impl
+
+  # MoE Slice Optimization to save memory
+  #export VLLM_SUPPORT_MOE_SLICE=True
+  #export VLLM_MOE_SLICE_LENGTH=8192
 fi
