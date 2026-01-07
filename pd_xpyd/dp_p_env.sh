68d3006fa761876b9271755c5d91aa0c16e8c689
Zhang, Yanli L <yanli.l.zhang@intel.com>
2026-01-07 17:52:57 +0800

    Enable FusedSDPA Slice for chunk prefil disable case
    Signed-off-by: Zhang, Yanli L <yanli.l.zhang@intel.com>

--- pd_xpyd/dp_p_env.sh
+++ pd_xpyd/dp_p_env.sh
@@ -16,6 +16,16 @@ input_min=128
 input_max=98304
 output_max=49152
 
+# Validated config for 64k with chunked prefill disabled based on fp8_inc
+#model_len=65536
+#max_num_batched_tokens=65536
+#max_num_seqs=8
+#input_min=128
+#input_max=65536
+#output_max=65536
+#export VLLM_GPU_MEMORY_UTILIZATION=0.25
+#export VLLM_GRAPH_RESERVED_MEM=0.115
+
 unset VLLM_CONTIGUOUS_PA VLLM_PADDING_AWARE_IN_CHUNKED_PREFILL
 CHUNKED_PREFILL_ENABLED=0
 
@@ -118,15 +128,15 @@ export VLLM_SUPPORT_MOE_CHUNK="false"  # Can be true after following para are tu
 if [ "$INC_FP8" -eq 1 ]; then
   export QUANT_CONFIG="$BASH_DIR"/inc_fp8_tp8ep8.json
 
-  # FA3 Env Settings - Added for fp8 inc only, not validated for static quant + bf16 kv cache
   export PT_HPU_SDPA_QKV_SLICE_MODE_FWD=1
-  export VLLM_HPU_FSDPA_SLICE_SEQ_LEN_THLD=4096
+  export VLLM_HPU_FSDPA_SLICE_SEQ_LEN_THLD=16384
   export PT_HPU_SDPA_BR_FACTOR=4096       # slice size on the query
   export PT_HPU_SDPA_BC_FACTOR=4096       # siice size on the kv
   export VLLM_HPU_FSDPA_SLICE_CHUNK_SIZE=4096 # qkv slice size in fp8 FSDPA
   export VLLM_HPU_FSDPA_SLICE_IMPL="slice_qkv"    # select the fp8 fsdpa impl
+  export VLLM_HPU_FSDPA_SLICE_CAUSAL="true"
 
-  # MoE Slice Optimization to save memory
-  #export VLLM_SUPPORT_MOE_SLICE=True
-  #export VLLM_MOE_SLICE_LENGTH=8192
+  # MoE Slice Optimization to save memory when chunked prefill isn't enabled for long model len(eg,64k)
+  export VLLM_SUPPORT_MOE_SLICE=True
+  export VLLM_MOE_SLICE_LENGTH=8192
 fi
