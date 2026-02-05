f0d83dd9ea4001f334c1cf711a3a27c8e9d6d68d
jkyu <jiankang.yu@intel.com>
2026-02-05 10:00:39 -0500

    Enable chunked prefill for qwenvl
    Signed-off-by: jkyu <jiankang.yu@intel.com>

--- vllm/attention/backends/hpu_attn.py
+++ vllm/attention/backends/hpu_attn.py
@@ -582,6 +582,9 @@ class HPUAttentionImpl(AttentionImpl, torch.nn.Module):
         decode_batch_size = 0
         decode_seq_len = 0
         decode_hidden_size = 0
+        query = query.squeeze(dim=0)
+        key = key.squeeze(dim=0)
+        value = value.squeeze(dim=0)
         if attn_metadata.num_prefills > 0:
             attn_data = self.preprocess_forward(
                 query[:attn_metadata.num_prefill_tokens],
